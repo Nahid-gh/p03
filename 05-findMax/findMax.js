2b06b761a6b51999d9680df25ce286d5f2a231b8
nanahid <ghabdianahid.gn@gmail.com>
2024-09-26 11:18:33 +0330

    practice5 is complete

---
+++ 05-findMax/findMax.js
@@ -0,0 +1,14 @@
+const list= [1, 17.8, 58.2, 56.59, 88.5, 28.4];
+console.log(Math.round(Math.max(...list)));
+
+
+const compareArray = function compare (list1 , list2){
+    for(let i=0; i<list1.length; i++){
+        if(list1.length !== list2.length)  return false;
+        if(list1[i] !== list2[i]) return false;
+        else if (console.log(true));
+    }
+};
+const list3 = [1, 2, 6, 8, 9.9]
+const list4 = [1, 2, 6, 8, 9.9]
+console.log(compareArray(list3,list4));
